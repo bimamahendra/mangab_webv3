9b12fb44ed44a4050d19452513ee8d665911b09e
s4ishoku <bimasaishoku@gmail.com>
2021-07-22 23:16:22 +0700

    tabel detail 2

--- application/views/absen/detail.php
+++ application/views/absen/detail.php
@@ -31,27 +31,27 @@
         <?php foreach ($detail_note as $note) {?>
           <tr>
             <th class="col-md-2">ID Absen</th>
-            <td class="col-md-1">:</td>
+            <td>:</td>
             <td class="col-md-3"><?php echo $note->ID_ABSEN;?></td>
           </tr>
           <tr>
             <th class="col-md-2">Kelas</th>
-            <td class="col-md-1">:</td>
+            <td>:</td>
             <td class="col-md-3"><?php echo $note->NAMA_MATKUL." - ".$note->KELAS_PRTMN;?></td>
           </tr>
           <tr>
             <th class="col-md-2">Topik</th>
-            <td class="col-md-1">:</td>
+            <td>:</td>
             <td class="col-md-3"><?php echo $note->TOPIK_ABSEN;;?></td>
           </tr>
           <tr>
             <th class="col-md-2">Metode</th>
-            <td class="col-md-1">:</td>
+            <td>:</td>
             <td class="col-md-3"><?php echo $note->METODE_ABSEN;?></td>
           </tr>
           <tr>
             <th class="col-md-2">Waktu</th>
-            <td class="col-md-1">:</td>
+            <td>:</td>
             <td class="col-md-3"><?php echo $note->TS_ABSEN;?></td>
           </tr>
           <?php }?>
