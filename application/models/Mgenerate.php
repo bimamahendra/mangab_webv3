8626745b0e83d28492984bfef00f76d4f26c5be8
s4ishoku <bimasaishoku@gmail.com>
2021-07-24 23:23:15 +0700

    note absen

--- application/models/Mgenerate.php
+++ application/models/Mgenerate.php
@@ -77,7 +77,7 @@ class Mgenerate extends CI_Model {
   }
 
   public function updatenote($note, $qr){
-    $this->db->set('NOTE', $note);
+    $this->db->set('NOTE_ABSEN', $note);
     $this->db->set('STATUS_ABSEN', 1);
     $this->db->where('ID_ABSEN', $qr);
     $this->db->update('absen');
